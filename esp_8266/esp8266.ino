a14d18737fab61cd9e7391c56ee4df4ae87dba6a
s2o2s <107086307+s2o2s@users.noreply.github.com>
2022-06-08 15:57:13 +0700

    Add files via upload

---
+++ esp_8266/esp8266.ino
@@ -0,0 +1,78 @@
+
+#include <ESP8266WiFi.h>
+#include "DHT.h"
+
+#define DHTPIN 0 // chan D3
+#define DHTTYPE DHT11 // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
+
+
+const char* ssid = "ten wifi";
+const char* password = "mat khau wifi";
+const uint16_t port = 8080;
+const char * host = "192.168.0.50";
+
+WiFiClient client_esp;
+void esp_connect()
+{
+  do{
+      client_esp.connect(host, port);
+      Serial.println("Connecting to server ......");
+      delay(1000);  
+    }while ( !client_esp.connected());
+    client_esp.print("ESP"); // thong bao cho server biet day la client do nhiet do do am
+  Serial.println("Connected to server successful!");  
+}
+int esp_check_connect()
+{
+  return client_esp.connected();
+}
+
+
+void setup()
+{ 
+  Serial.begin(115200);
+  
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("...");
+  }
+  Serial.print("WiFi connected with IP: ");
+  Serial.println(WiFi.localIP());
+  
+  dht.begin(); 
+  esp_connect(); 
+  delay(2000);
+  dht.readHumidity();
+  dht.readTemperature();
+  delay(1000);
+}
+ 
+void loop()
+{
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+
+    if (!esp_check_connect())
+    {
+      esp_connect();
+      delay(2000);
+      return;    
+    }
+    else
+    {
+      client_esp.print("esp8266");  //
+      if (isnan(h) || isnan(t))
+      {
+        client_esp.print(-999);  // gui thong bao loi doc nhiet do do am
+      }
+      else
+      {
+        client_esp.print(t);
+        client_esp.print("%"); // phan cach nhiet do do am
+        client_esp.print(h);
+      }    
+    }
+    delay(5000);
+}
---
+++ server_client/server.py
@@ -0,0 +1,118 @@
+
+import socket
+from time import sleep
+import threading
+import datetime
+
+server = socket.socket()
+server.bind(('0.0.0.0', 8080))
+server.listen(10)
+
+
+temp = 0
+humi = 0
+n = 30
+Arr_Temp_Humi_Time = []
+
+
+def Update_value(t,h):
+    if t == -1 or h == -1:
+        return
+    now_time = datetime.datetime.now().strftime("%H:%M:%S")
+    if len(Arr_Temp_Humi_Time) < n:
+        Arr_Temp_Humi_Time.append((t,h,now_time,))
+    else:
+        for i in range(0,n-1):
+            Arr_Temp_Humi_Time[i] = Arr_Temp_Humi_Time[i+1]
+        Arr_Temp_Humi_Time[n-1] = (t,h,now_time,)
+
+
+def Send_value(client,lst):
+    sleep(0.1)
+    client.sendall(str(len(lst)).encode())
+    for i in range(0,len(lst)):
+        for j in range(0,3):
+            sleep(0.01)
+            client.sendall(str(lst[i][j]).encode())
+
+
+def Handle_Client_Esp(client_esp):
+    def Thread_Client_Esp():
+        global temp
+        global humi
+        while True:
+            temp = -1
+            humi = -1
+            try:
+                content = client_esp.recv(128)
+                if content == b'esp8266':
+                    content1 = client_esp.recv(128)
+                    if content1 == b'-999':
+                        print("Lỗi đọc cảm biến")
+                    else:
+                        #print(content1)
+                        content1 = content1.decode()
+                        value = content1.split('%')
+                        temp = float(value[0])
+                        humi = float(value[1])
+            except Exception as e:
+                print(e)
+                print("Mất kết nối")
+                client_esp.close()
+                return
+            Update_value(temp,humi)
+            sleep(1)
+
+    threading.Thread(target=Thread_Client_Esp, daemon=True).start()
+
+
+def Handle_Client_User(client_user):
+    def Thread_Client_User():
+        while True:
+            try:
+                request = client_user.recv(128).decode()
+                if request == 'get_list':
+                    client_user.sendall(b'1')
+                    sleep(0.5)
+                    Send_value(client_user,Arr_Temp_Humi_Time)
+                    continue
+                if len(Arr_Temp_Humi_Time)==0:
+                    client_user.sendall(b'999')
+                else:
+                    client_user.sendall(b'2')
+                    sleep(0.5)
+                    client_user.sendall(str(Arr_Temp_Humi_Time[len(Arr_Temp_Humi_Time)-1][0]).encode())
+                    sleep(0.05)
+                    client_user.sendall(str(Arr_Temp_Humi_Time[len(Arr_Temp_Humi_Time)-1][1]).encode())
+                    sleep(0.05)
+                    client_user.sendall(str(Arr_Temp_Humi_Time[len(Arr_Temp_Humi_Time)-1][2]).encode())
+            except Exception as e:
+                print(e)
+                client_user.close()
+                return
+            sleep(1)
+    threading.Thread(target=Thread_Client_User,daemon=True).start()
+
+
+while True:
+    try:
+        client, addr = server.accept()
+        client.settimeout(10)
+
+        type_client = client.recv(128)
+        if type_client == b'ESP':
+            Handle_Client_Esp(client)
+        elif type_client == b'USER':
+            Handle_Client_User(client)
+        else:
+            print('Từ chối kết nối')
+            client.close()
+
+    except Exception as e:
+        print(e)
+    sleep(1)
+
+
+
+
+
